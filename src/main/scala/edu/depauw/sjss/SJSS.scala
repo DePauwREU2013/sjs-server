d50cc67f6a735b6059535f0bd2d279b43d12fa1a
Brian Howard <bhoward@depauw.edu>
2014-07-22 23:57:00 -0400

    Trying to figure out Scalatra's JSON handling for POST.

--- src/main/scala/edu/depauw/sjss/SJSS.scala
+++ src/main/scala/edu/depauw/sjss/SJSS.scala
@@ -6,18 +6,55 @@ import scalate.ScalateSupport
 class SJSS extends SJSServerStack {
 
   get("/") {
+    val submitScript = scala.xml.Unparsed(
+      """
+      | $("#testform").submit(function(e) {
+      |   var files = {
+      |     "files": {
+      |       "fileone.scala": {
+      |         "filename": "fileone.scala",
+      |         "type": "text/plain",
+      |         "content": $("input[name='fileone']").val()
+      |       },
+      |       "filetwo.scala": {
+      |         "filename": "filetwo.scala",
+      |         "type": "text/plain",
+      |         "content": $("input[name='filetwo']").val()
+      |       }
+      |     }
+      |   };
+      |   alert(JSON.stringify(files));
+      |   var callback = function() {};
+      |   $.ajax({
+      |     type: "POST",
+      |     url: "compile",
+      |     data: JSON.stringify(files),
+      |     success: callback,
+      |     dataType: "json",
+      |     contentType: "application/json",
+      |     processData: false
+      |   });
+      |   return false;
+      | })
+      """.stripMargin)
+      
     <html>
       <head>
+      <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
       <script src="foo"></script>
       </head>
       <body>
         <h1>Hello, world!</h1>
         Say <a href="hello-scalate">hello to Scalate</a>.
-        <form action="compile" method="POST">
+        <form id="testform">
           <input type="submit" value="Push Me" />
-          <input type="text" name="files" />
+          <input type="text" name="fileone" value="file one" />
+          <input type="text" name="filetwo" value="file two" />
         </form>
         <canvas id="output"></canvas>
+        <script>
+          { submitScript }
+        </script>
       </body>
     </html>
   }
@@ -28,6 +65,7 @@ class SJSS extends SJSServerStack {
   }
   
   post("/compile") {
+    println(params)
     val files = params("files") // TODO how to get the map?
     println(files)
     
