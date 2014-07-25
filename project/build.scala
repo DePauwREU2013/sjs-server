5a102bc4a3cfebcffef807cc1a2a13ba2aaf928d
Brian Howard <bhoward@depauw.edu>
2014-07-24 22:54:52 -0400

    Co-opting the compiler interface from scala-js-fiddle.

--- project/build.scala
+++ project/build.scala
@@ -27,6 +27,13 @@ object SjsserverBuild extends Build {
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "org.scalatra" %% "scalatra-json" % "2.3.0",
         "org.json4s" %% "json4s-jackson" % "3.2.9",
+        "org.scala-lang" % "scala-compiler" % scalaVersion.value,
+        "com.typesafe.akka" %% "akka-actor" % "2.3.2",
+        "org.scala-lang.modules.scalajs" % s"scalajs-compiler_${scalaVersion.value}" % "0.5.0",
+        "org.scala-lang.modules.scalajs" %% "scalajs-tools" % "0.5.0",
+        "org.scala-lang.modules" %% "scala-async" % "0.9.1" % "provided",
+        "com.scalatags" %% "scalatags" % "0.3.8",
+        "com.lihaoyi" %% "acyclic" % "0.1.2" % "provided",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
---
+++ src/main/scala/fiddle/Shared.scala
@@ -0,0 +1,40 @@
+package fiddle
+
+import scala.annotation.ClassfileAnnotation
+
+object Shared{
+  val prelude =
+    """
+      |import scalatags.JsDom.all._
+      |import org.scalajs.dom
+      |import fiddle.Page
+      |import Page.{red, green, blue, println}
+      |import scalajs.js
+    """.stripMargin
+
+  val default = """
+    |import scalajs.js
+    |object ScalaJSExample extends js.JSApp{
+    |  def main() = {
+    |    println("Looks like there was an error loading the default Gist!")
+    |    println("Loading an empty application so you can get started")
+    |  }
+    |}
+  """.stripMargin
+
+  val gistId = "9443f8e0ecc68d1058ad"
+
+  val url = "http://localhost:8080"
+}
+
+class Web extends ClassfileAnnotation
+
+@Web
+trait Api{
+  def compile(txt: String): (String, Option[String])
+  def fastOpt(txt: String): (String, Option[String])
+  def fullOpt(txt: String): (String, Option[String])
+  def export(compiled: String, source: String): String
+  def `import`(compiled: String, source: String): String
+  def completeStuff(txt: String, flag: String, offset: Int): List[(String, String)]
+}
