d08b6ba55ac5089fea4582d0049964d0640ae035
Brian Howard <bhoward@depauw.edu>
2014-07-30 18:14:07 -0400

    Removed Scalate

--- src/main/scala/edu/depauw/sjss/SJSServerStack.scala
+++ src/main/scala/edu/depauw/sjss/SJSServerStack.scala
@@ -7,31 +7,12 @@ import org.fusesource.scalate.layout.DefaultLayoutStrategy
 import javax.servlet.http.HttpServletRequest
 import collection.mutable
 
-trait SJSServerStack extends ScalatraServlet with ScalateSupport {
-
-  /* wire up the precompiled templates */
-  override protected def defaultTemplatePath: List[String] = List("/WEB-INF/templates/views")
-  override protected def createTemplateEngine(config: ConfigT) = {
-    val engine = super.createTemplateEngine(config)
-    engine.layoutStrategy = new DefaultLayoutStrategy(engine,
-      TemplateEngine.templateTypes.map("/WEB-INF/templates/layouts/default." + _): _*)
-    engine.packagePrefix = "templates"
-    engine
-  }
-  /* end wiring up the precompiled templates */
-  
-  override protected def templateAttributes(implicit request: HttpServletRequest): mutable.Map[String, Any] = {
-    super.templateAttributes ++ mutable.Map.empty // Add extra attributes here, they need bindings in the build file
-  }
-  
-
+trait SJSServerStack extends ScalatraServlet {
+  // Removed Scalate stuff
   notFound {
     // remove content type in case it was set through an action
     contentType = null
-    // Try to render a ScalateTemplate if no route matched
-    findTemplate(requestPath) map { path =>
-      contentType = "text/html"
-      layoutTemplate(path)
-    } orElse serveStaticResource() getOrElse resourceNotFound()
+
+    serveStaticResource() getOrElse resourceNotFound()
   }
 }
